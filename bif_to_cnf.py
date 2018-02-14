e12450c947fe8ef01734648da94fba9cd40b796f
Timothy Werquin <timothy@werquin.com>
2018-02-14 15:40:50 +0100

    Add enc2 support (and output support for both cachet and c2d)

--- bif_to_cnf.py
+++ bif_to_cnf.py
@@ -1,10 +1,76 @@
+#!/usr/bin/python3
+
 import sys
+import argparse
 import itertools
 
 from deps.bif_parser import BIFParser as BIFP
 
+
+# variables are tuple: (node name, var state name)
+# conditional variables have an extra tuple: (node name, var state name, (conditional var 1, ...))
+# for cnf: clauses are tuples (Boolean, variable)
+# with boolean == False <=> not(var)
+# and variable as defined above
+
+def get_combinations(list_of_lists):
+    return itertools.product(*list_of_lists)
+
+def create_var(node, state):
+    return (node.getName(), state)
+
+def create_conditional_var(node, state, conds):
+    return (node.getName(), state, tuple(conds))
+
+def var_cnf(var, true=True):
+    return (true, var)
+
+def get_state_vars(node):
+    return [create_var(node, s) for s in node.getStates()]
+
+def get_all_cond_vars(node):
+    cond_list = [node.getStates()]
+    for parent in node.getParents():
+        cond_list.append(parent.getStates())
+    pairs = get_combinations(cond_list)
+    return [create_conditional_var(node, p[0], p[1:]) for p in pairs]
+
+def create_variables(nodes, enc1):
+    variables = {}
+    nvars = 0
+    for node in nodes:
+        name = node.getName()
+        states = node.getStates()
+
+        # each state gets one variable
+        svars = get_state_vars(node)
+
+        parents = node.getParents()
+        # per state and state of each parent -> one variable
+        if enc1:
+            cond_list = [states]
+        else:
+            cond_list = [states[:-1]]
+
+        for parent in parents:
+            cond_list.append(parent.getStates())
+
+        pairs = get_combinations(cond_list)
+        pvars = [create_conditional_var(node, p[0], p[1:]) for p in pairs]
+
+        for v in svars + pvars:
+            variables[v] = nvars + 1
+            nvars += 1
+
+    return variables, nvars
+
+def not_cnf(x):
+    """ Negates the given cnf variable """
+    return (not x[0], x[1])
+
 def notOfAnds(ands):
-    return ['-' + x for x in ands]
+    """ maps not_cnf to the given list of ands turning it into an or """
+    return [not_cnf(x) for x in ands]
 
 def convImplToCnf(left, right):
     if len(right) > 1:
@@ -17,90 +83,146 @@ def convEqToCnf(left, right):
     impl2 = convImplToCnf(right, left)
     return impl1 + impl2
 
-def cnfToInts(cnf, vs):
+
+def create_indicator_cnf(node):
+    cnf = []
+    svars = get_state_vars(node)
+    # the disjunction of all states:
+    cnf.append([var_cnf(v) for v in svars])
+
+    # negation:
+    l = len(svars)
+    for j in range(l):
+        for i in range(j):
+            cnf.append([var_cnf(svars[i], False), var_cnf(svars[j], False)])
+    return cnf
+
+
+def toEnc1(nodes):
+    cnf = []
+    for node in nodes:
+        cnf += create_indicator_cnf(node)
+
+        parents = node.getParents()
+
+        cond_list = [node.getStates()]
+        for parent in parents:
+            cond_list.append(parent.getStates())
+        pairs = get_combinations(cond_list)
+
+        nodes = [node] + parents
+
+        for pair in pairs:
+            rl = var_cnf(create_conditional_var(node, pair[0], pair[1:])) #name + '.cv.' + '|'.join(pair)
+            ll = [var_cnf(create_var(nodes[i], s)) for i, s in enumerate(pair)] # [pnames[i] + '.v.' + s for i, s in enumerate(pair)]
+            #print(convEqToCnf(ll, [rl]))
+            cnf += convEqToCnf(ll, [rl])
+    return cnf
+
+
+def toEnc2(nodes):
+    cnf = []
+    for node in nodes:
+        states = node.getStates()
+
+        cnf += create_indicator_cnf(node)
+
+        cond_list = [states]
+        for parent in node.getParents():
+            cond_list.append(parent.getStates())
+        pairs = itertools.product(*cond_list)
+
+        nodes = [node] + node.getParents()
+
+        for pair in pairs:
+            ll = [var_cnf(create_var(nodes[i+1], s)) for i, s in enumerate(pair[1:])]
+            ll += [var_cnf(create_conditional_var(node, s, pair[1:]), False) for s in states[:states.index(pair[0])]]
+            if pair[0] != states[-1]:
+                ll += [var_cnf(create_conditional_var(node, pair[0], pair[1:]))]
+
+            rl = var_cnf(create_var(node, pair[0]))
+            #print(ll, '=>', rl)
+            cnf += convImplToCnf(ll, [rl])
+
+    return cnf
+
+
+def cnfToInts(cnf, variables):
     ret = ""
     for conj in cnf:
         line = ""
         for disj in conj:
-            if disj[0] == '-':
-                line += str(-vs[disj[1:]])
-            else:
-                line += str(vs[disj])
-            line += " "
+            i = variables[disj[1]]
+            if not disj[0]:
+                i = -i
+            line += str(i) + " "
         line += "0\n"
         ret += line
     return ret
 
-def enc1(nodes):
-    cnf = []
+def assign_weights_enc1(nodes):
+    weights = {}
     for node in nodes:
-        name = node.getName()
-        states = node.getStates()
-        parents = node.getParents()
-        svars = [name + '.v.' + s for s in states]
+        cpd = node.getDist()
+        cvars = get_all_cond_vars(node)
 
-        cnf.append(list(svars))
-        l = len(svars)
-        for j in range(l):
-            for i in range(j):
-                cnf.append(['-' + svars[i], '-' + svars[j]])
+        for cvar in cvars:
+            prob = cpd[(cvar[1],) + cvar[2]]
+            weights[cvar] = prob
+    return weights
 
-        cond_list = [states]
-        for parent in parents:
-            cond_list.append(parent.getStates())
-        pairs = itertools.product(*cond_list)
-        
-        pnames = [name] + [p.getName() for p in parents]
-
-        for pair in pairs:
-            rl = name + '.cv.' + '|'.join(pair)
-            ll = [pnames[i] + '.v.' + s for i, s in enumerate(pair)]
-            #print(convEqToCnf(ll, [rl]))
-            cnf += convEqToCnf(ll, [rl])    
-    return cnf
-
-
-def enc2(nodes):
-    cnf = []
+def assign_weights_enc2(nodes):
+    weights = {}
     for node in nodes:
-        name = node.getName()
         states = node.getStates()
-        parents = node.getParents()
-        svars = [name + '.v.' + s for s in states]
+        cpd = node.getDist()
+        cvars = get_all_cond_vars(node)
 
-        cnf.append(list(svars))
-        l = len(svars)
-        for j in range(l):
-            for i in range(j):
-                cnf.append(['-' + svars[i], '-' + svars[j]])
+        for cvar in cvars:
+            prob = cpd[(cvar[1],) + cvar[2]]
+            # index of state
+            idx = states.index(cvar[1])
+            divisor = 1
+            for i in range(idx):
+                divisor -= cpd[(states[i],) + cvar[2]]
 
-        cond_list = [states]
-        for parent in parents:
-            cond_list.append(parent.getStates())
-        pairs = itertools.product(*cond_list)
-        
-        pnames = [name] + [p.getName() for p in parents]
+            weights[cvar] = prob / divisor
+    return weights
 
-        for pair in pairs:
-            if pair[0] == states[-1]:
-            else:
-                ll = [pnames[i] + '.v.' + s for i, s in enumerate(pair)]
-            #rl = name + '.cv.' + '|'.join(pair)
-            #ll = [pnames[i] + '.v.' + s for i, s in enumerate(pair)]
-            #print(convEqToCnf(ll, [rl]))
-            #cnf += convEqToCnf(ll, [rl])    
-        
-    return cnf
+def weights_to_str(weights, variables, enc1, c2d):
+    ws = {}
+    for var in variables:
+        if var in weights:
+            weight = weights[var]
+            ws[variables[var]] = (weight, 1 if enc1 else (1 - weight))
+        else:
+            ws[variables[var]] = (1 if c2d else -1, 1)
+
+    wline = ""
+    if c2d:
+        wline += "c weights"
+        for i in range(len(variables)):
+            wline += " " + str(ws[i+1][0]) + " " + str(ws[i+1][1])
+        wline += '\n'
+    else:
+        for i in range(len(variables)):
+            wline += "w " + str(i+1) + " " + str(ws[i+1][0]) + "\n"
+    return wline
 
 def main():
-    if len(sys.argv) != 3:
-        print("USAGE: ", sys.argv[0], " bif-file cnf-out-file")
-        return -1
+    parser = argparse.ArgumentParser(description="Convert a baysean network to CNF")
+    parser.add_argument("--enc", "-e", default=1, type=int)
+    parser.add_argument("--cnf-type", default="c2d", type=str)
+    parser.add_argument("bif_file")
+    parser.add_argument("cnf_file")
 
-    bif_file_name = sys.argv[1]
-    cnf_file_name = sys.argv[2]
-    
-    enc1 = False
+    args = parser.parse_args()
+
+    bif_file_name = args.bif_file
+    cnf_file_name = args.cnf_file
+
+    enc1 = args.enc == 1
+    cnf_type = args.cnf_type
 
     nodes = None
     with open(bif_file_name, "r") as f:
@@ -117,85 +239,36 @@ def main():
         n.printNode()
 
     # create variables
-    # map from name to int?
-    variables = {}
-    nvars = 0
-    for node in nodes:
-        name = node.getName()
-        states = node.getStates()
-        
-        # each state gets one variable
-        svars = [name + '.v.' + s for s in states]
-
-        parents = node.getParents()
-        # per state and state of each parent -> one variable
-        if enc1:
-            cond_list = [states]
-        else:
-            cond_list = [states[:-1]]        
-        for parent in parents:
-            cond_list.append(parent.getStates())
-
-        pairs = itertools.product(*cond_list)
-        pvars = [name + '.cv.' + '|'.join(p) for p in pairs]
-
-        for v in svars + pvars:
-            variables[v] = nvars + 1
-            nvars += 1
+    # map from name to int
+    variables, nvars = create_variables(nodes, enc1)
 
     print("variables:")
     for v in variables:
         print(v, "=", variables[v])
 
     # create cnf
-    
-    cnf = enc1(nodes) if enc1 else enc2(nodes)
-            
+    cnf = toEnc1(nodes) if enc1 else toEnc2(nodes)
+
     print("cnf:")
     for c in cnf:
         print(c)
 
     # assign weights
-    weights = {}
-    for node in nodes:
-        name = node.getName()
-        states = node.getStates()
-        parents = node.getParents()
+    weights = assign_weights_enc1(nodes) if enc1 else assign_weights_enc2(nodes)
 
-        cond_list = [states]
-        for parent in parents:
-            cond_list.append(parent.getStates())
-        pairs = itertools.product(*cond_list)
-        
-        cpd = node.getDist()
-
-        for pair in pairs:
-            var_name = name + '.cv.' + '|'.join(pair)
-            prob = cpd[pair]
-            weights[var_name] = prob
     print("weights:")
     print(weights)
     # save cnf
-    
+
     with open(cnf_file_name, "w") as f:
         f.write("c " + cnf_file_name + "\n")
         f.write("c " + bif_file_name + "\n")
         f.write("c\n")
-        
-        f.write("p cnf " + str(nvars) + " " + str(len(cnf)) + "\n")
-        
 
-        ws = {}
-        for var in variables:
-            if var in weights:
-                ws[variables[var]] = (weights[var], 1)
-            else:
-                ws[variables[var]] = (1, 1)
-                #f.write("w " + str(variables[var]) + " -1\n")
-        wline = "c weights"
-        for i in range(nvars):
-            wline += " " + str(ws[i+1][0]) + " " + str(ws[i+1][1])
-        f.write(wline + "\n")
+        f.write("p cnf " + str(nvars) + " " + str(len(cnf)) + "\n")
+
+        wline = weights_to_str(weights, variables, enc1, cnf_type == "c2d")
+        f.write(wline)
         f.write(cnfToInts(cnf, variables))
     return 0
 
