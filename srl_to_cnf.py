ddc612602f81c5459dc12baac0d4ac5d500fe678
Floni <florianvanheghe@gmail.com>
2018-02-16 15:35:18 +0100

    Added test.py to convert problog file to CNF. WIP

--- srl_to_cnf.py
+++ srl_to_cnf.py
@@ -3,9 +3,9 @@
 import sys
 import argparse
 import subprocess
+import problog
 
 def main():
-    print("test")
     parser = argparse.ArgumentParser(description="srl to CNF")
     parser.add_argument("pl_file")
     parser.add_argument("cnf_file")
@@ -15,9 +15,12 @@ def main():
     pl_file_name = args.pl_file
     cnf_file_name = args.cnf_file
 
-    grounded = subprocess.Popen(['problog', 'ground', pl_file_name])
+    grounded = subprocess.Popen(['problog', 'ground', pl_file_name],stdout=subprocess.PIPE)
 
-    print(grounded)
+    for line in grounded.stdout:
+        linestr = line.decode().rstrip()
+        print(linestr)
+
 
     return 0
 
